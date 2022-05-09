bc7e22e0152b1de29a29d7ae25bf3168ab550071
NoobGajen <sudogajendra20030106@gmail.com>
2022-05-09 16:41:36 +0545

    Edited

---
+++ build/precache-manifest.428e8931873e701a85da7d48c167c4c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4964ae098616c6534448",
+    "url": "/gajendrasite/static/css/main.52eba9e7.chunk.css"
+  },
+  {
+    "revision": "4964ae098616c6534448",
+    "url": "/gajendrasite/static/js/main.4964ae09.chunk.js"
+  },
+  {
+    "revision": "247ee42e9569f204b9aa",
+    "url": "/gajendrasite/static/js/1.247ee42e.chunk.js"
+  },
+  {
+    "revision": "1d8eb5babb328df5d859",
+    "url": "/gajendrasite/static/js/runtime~main.1d8eb5ba.js"
+  },
+  {
+    "revision": "9ae4a029b29582a91d978686806b2458",
+    "url": "/gajendrasite/index.html"
+  }
+];
